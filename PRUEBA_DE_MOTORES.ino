ac85148b0f9bd0120219f06030ce6c248951b9de
claupons <39241747+claupons@users.noreply.github.com>
2018-05-13 09:57:29 -0600

    Add files via upload

---
+++ PRUEBA_DE_MOTORES.ino
@@ -0,0 +1,22 @@
+#include<AFMotor.h>
+AF_DCMotor motor1(1);
+AF_DCMotor motor2(2);
+AF_DCMotor motor3(3);
+AF_DCMotor motor4(4);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  motor1.setSpeed(1000);
+  motor2.setSpeed(1000);
+  motor3.setSpeed(1000);
+  motor4.setSpeed(1000);
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+  delay(1000);
+}
---
+++ Programaci_n_final.ino
@@ -0,0 +1,98 @@
+int redLed = 12;
+int greenLed = 11;
+int smokeA0 = A0;
+// Your threshold value
+int sensorThres = 400;
+const int sensorPIN = A2;
+const int sampleWindow = 50; // Ancho ventana en mS (50 mS = 20Hz)
+ const int sensorMin = 4;     // lectura mínima
+const int sensorMax = 1024;  // lectura máxima
+
+void setup() {
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  pinMode(smokeA0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int analogSensor = analogRead(smokeA0);
+
+ 
+  
+  // Checks if it has reached the threshold value
+  if (analogSensor > sensorThres)
+  {
+    digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    Serial.println ("Hay fuego");
+  }
+  else
+  {
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, HIGH);
+    Serial.println (" "); 
+    unsigned long startMillis= millis();
+ 
+   unsigned int signalMax = 0;
+   unsigned int signalMin = 1024;
+ 
+   // Recopilar durante la ventana
+   unsigned int sample;
+   while (millis() - startMillis < sampleWindow)
+   {
+      sample = analogRead(sensorPIN);
+      if (sample < 1024)
+      {
+         if (sample > signalMax)
+         {
+            signalMax = sample;  // Actualizar máximo
+         }
+         else if (sample < signalMin)
+         {
+            signalMin = sample;  // Actualizar mínimo
+         }
+      }
+   }
+   unsigned int peakToPeak = signalMax - signalMin;  // Amplitud del sonido
+   double volts = (peakToPeak * 5.0) / 1024;  // Convertir a tensión
+   
+   if (volts>0.1)
+{ 
+  Serial.println ("Estan talando un árbol");
+   digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+}
+else
+  {
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, HIGH);
+    Serial.println (" ");
+  // Leer el sensor en el pin A0:
+  int sensorReading = analogRead(A4);
+  // Mapeamos el rango del sensor (4 opciones):
+  // f¿Función: 'long int map(long int, long int, long int, long int, long int)'
+  int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
+  
+  // Valores del rango:
+  switch (range) {
+  case 1:    // Fuego cercano, a una distancia de aproximadamente 1.5 pies.
+    Serial.println("** HAY FUEGO **");
+     digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    break;
+  case 0:    // Se detecta fuego entre 1-3 pies de distancia.
+    Serial.println("** HAY FUEGO **");
+     digitalWrite(redLed, HIGH);
+    digitalWrite(greenLed, LOW);
+    break;
+  case 2:    // No se ha detectado fuego.
+    Serial.println("");
+    digitalWrite(redLed, LOW);
+    digitalWrite(greenLed, HIGH);
+    break;
+     }
+  }
+  delay(100);
+ }
+ }
